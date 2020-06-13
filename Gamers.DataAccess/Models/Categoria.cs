f20e7ddb993305c2924f7da5e504e03bf152063e
Leonel Quiroga <m_leonelq@hotmail.com>
2020-06-13 16:54:04 -0300

    Agrego Migrations Agrego Context Agrego Entidad Categoria

---
+++ Gamers.DataAccess/Models/Categoria.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gamers.DataAccess.Models
+{
+    
+     public  class Categoria
+    {   
+        public int Id { get; set; }
+       
+        public string Descripcion { get; set; }
+
+        public int? EdadMinima { get; set; }
+
+    }
+}
