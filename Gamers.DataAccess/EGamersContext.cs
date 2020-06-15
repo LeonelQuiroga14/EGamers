826c7a7630278eb27e1a3bbafd58b5c251966342
IgnacioMendia <65725528+IgnacioMendia@users.noreply.github.com>
2020-06-14 22:14:59 -0300

    ABM Categorias

--- Gamers.DataAccess/EGamersContext.cs
+++ Gamers.DataAccess/EGamersContext.cs
@@ -1,8 +1,10 @@
 ﻿using Gamers.DataAccess.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Configuration;
 using System.Data.Entity;
+using System.Data.Entity.Infrastructure.Annotations;
 using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
@@ -24,11 +26,13 @@ namespace Gamers.DataAccess
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
             #region Categoria
             modelBuilder.Entity<Categoria>().HasKey(x => x.Id)
-                .Property(x => x.Descripcion).IsRequired().HasColumnType("nvarchar").HasMaxLength(50);
-                                    
+                .Property(x => x.Descripcion).IsRequired().HasColumnType("nvarchar").HasMaxLength(50)
+           .HasColumnAnnotation(IndexAnnotation.AnnotationName, new IndexAnnotation(new IndexAttribute("IX_DescripcionUnica") {IsUnique=true}));
+
             #endregion
         }
 
         public DbSet<Categoria> Categoria { get; set; }
+        public DbSet<Juego> Juego { get; set; }
     }
 }
--- Gamers.Web/Controllers/CategoriaController.cs
+++ Gamers.Web/Controllers/CategoriaController.cs
@@ -1,4 +1,5 @@
 ﻿using Gamers.DataAccess;
+using Gamers.DataAccess.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,6 +22,125 @@ namespace Gamers.Web.Controllers
             return View(lista);
         }
 
+        public ActionResult Alta()
+        {
+            return View();
+        }
 
+        [HttpPost]
+        public ActionResult Alta(Categoria categoria)
+        {
+            if (!ModelState.IsValid)
+            {
+                //ModelState.AddModelError("", "Pone todo pelotudo");
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            try
+            {                  
+                _context.Categoria.Add(categoria);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                if (ex.InnerException != null && ex.InnerException.InnerException != null && ex.InnerException.InnerException.Message.Contains("IX_"))
+                {
+                    ModelState.AddModelError(string.Empty, "Categoria existente");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                }
+                return View(categoria);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost, ActionName("Baja")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Baja(int id)
+        {           
+            var Categoria=_context.Categoria.Find(id);
+            if (Categoria==null)
+            {
+                return HttpNotFound();
+            }
+            try
+            {
+                _context.Categoria.Remove(Categoria);
+            }
+            catch (Exception ex)
+            {
+                if (ex.InnerException != null && ex.InnerException.InnerException != null && ex.InnerException.InnerException.Message.Contains("Reference"))
+                {
+                    ModelState.AddModelError(string.Empty,"La categoria tiene datos en otra tabla");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                }
+                return View(Categoria);
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+            
+        }
+        [HttpGet]
+        public ActionResult Baja(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            var Categoria = _context.Categoria.Find(id);
+            if (Categoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(Categoria);
+        }
+
+        [HttpGet]
+        public ActionResult Modificar(int? id)
+        {
+            if (id==null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            var Categoria =_context.Categoria.Find(id);
+            if (Categoria==null)
+            {
+                return HttpNotFound();
+            }
+            return View(Categoria);
+        }
+
+        [HttpPost]
+        public ActionResult Modificar(Categoria categoria)
+        {
+            if (!ModelState.IsValid)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            try
+            {
+                _context.Entry(categoria).State = System.Data.Entity.EntityState.Modified;
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            { 
+                if (ex.InnerException != null && ex.InnerException.InnerException != null && ex.InnerException.InnerException.Message.Contains("IX_"))
+                {
+                    ModelState.AddModelError(string.Empty, "Ya existe gil");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                }
+                return View(categoria);
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
