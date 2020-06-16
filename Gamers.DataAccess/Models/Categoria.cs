e6aa39d463a56c3b84399d97964a17532c698df1
IgnacioMendia <65725528+IgnacioMendia@users.noreply.github.com>
2020-06-16 12:41:30 -0300

    ABM juegos

--- Gamers.DataAccess/Models/Categoria.cs
+++ Gamers.DataAccess/Models/Categoria.cs
@@ -11,6 +11,7 @@ namespace Gamers.DataAccess.Models
      public  class Categoria
      {   
         public int Id { get; set; }
+
         [Required(ErrorMessage = "El campo es obligatorio.")]
         [StringLength(50, ErrorMessage = "El campo debe contener entre {2} y {1} caracteres.", MinimumLength = 3)]
         [Display(Name = "Nombre de categoria")]
